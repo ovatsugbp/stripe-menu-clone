f6ec2c0807bad308ec2db71ec424a2e0a010372f
Gustavo G <gustavogoullart@hotmail.com>
2021-01-13 19:41:57 -0300

    first commit

---
+++ src/Hero.js
@@ -0,0 +1,27 @@
+import React from "react";
+import phoneImg from "./images/phone.svg";
+import { useGlobalContext } from "./context";
+
+const Hero = () => {
+    const { closeSubmenu } = useGlobalContext();
+    return (
+        <section className="hero" onMouseOver={closeSubmenu}>
+            <div className="hero-center">
+                <article className="hero-info">
+                    <h1>Payments infrastructure for the internet</h1>
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Nostrum praesentium inventore incidunt aperiam
+                        fugiat repellat voluptates quos et, vero eligendi.
+                    </p>
+                    <button className="btn">Start Now</button>
+                </article>
+                <article className="hero-images">
+                    <img src={phoneImg} alt="phone" className="phone-img" />
+                </article>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
---
+++ src/Sidebar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { FaTimes } from "react-icons/fa";
+import sublinks from "./data";
+import { useGlobalContext } from "./context";
+
+const Sidebar = () => {
+    const { closeSidebar, isSidebarOpen } = useGlobalContext();
+
+    return (
+        <aside
+            className={`${
+                isSidebarOpen ? "sidebar-wrapper show" : "sidebar-wrapper"
+            }`}
+        >
+            <div className="sidebar">
+                <button className="close-btn" onClick={closeSidebar}>
+                    <FaTimes />
+                </button>
+                <div className="sidebar-links">
+                    {sublinks.map((items, index) => {
+                        const { links, page } = items;
+                        return (
+                            <article key={index}>
+                                <h4>{page}</h4>
+                                <div className="sidebar-sublinks">
+                                    {links.map((el, index) => {
+                                        const { label, icon, url } = el;
+                                        return (
+                                            <a href={url} key={index}>
+                                                {icon}
+                                                {label}
+                                            </a>
+                                        );
+                                    })}
+                                </div>
+                            </article>
+                        );
+                    })}
+                </div>
+            </div>
+        </aside>
+    );
+};
+
+export default Sidebar;
